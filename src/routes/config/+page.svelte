a29aeba773cfea213745c431eac3533f487c436e
Nico J. <nicolas.javelaud@mistral.ai>
2025-07-31 13:15:44 +0200

    Hide Color input

--- src/routes/config/+page.svelte
+++ src/routes/config/+page.svelte
@@ -3,14 +3,12 @@
     import Input from "@/components/ui/input/input.svelte";
     import Label from "@/components/ui/label/label.svelte";
     import { config } from "@/config.svelte";
-
-
 </script>
 
 <div class="flex flex-col h-full w-full gap-4 px-4 pb-4">
     <Label>Company name</Label>
     <Input placeholder="App name" bind:value={config.company.name} />
-    <Label>Brand color (hex)</Label>
-    <Input placeholder="Color" bind:value={config.company.color} />
+    <!-- <Label>Brand color (hex)</Label>
+    <Input placeholder="Color" bind:value={config.company.color} /> -->
     <Button href="/">Back home</Button>
 </div>
